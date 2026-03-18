a584b3b12f95edf4b75b79ef3b3b3a68befd4e74
Maria <mashadanchenko75@gmail.com>
2026-03-18 18:18:34 +0300

    feat(): init project

--- src/test/java/tests/BoardsTests.java
+++ src/test/java/tests/BoardsTests.java
@@ -19,9 +19,7 @@ public class BoardsTests extends BaseTest {
 
     @Test(priority = 2)
     public void testAddCard() {
-        driver.get("https://trello.com/b/Z18mRpRD/%D0%BC%D0%BE%D1%8F-%D0%B4%D0%BE%D1%81%D0%BA%D0%B0-trello");
-
-        BoardPage boardPage = new BoardPage(driver);
+               BoardPage boardPage = new BoardPage(driver);
         boardPage.addCard("Сегодня", "Тестовая карточка");
 
         Assert.assertTrue(boardPage.isCardDisplayedInList("Сегодня", "Тестовая карточка"));
